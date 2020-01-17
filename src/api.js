90c61e62d36e1acab1fafd66c3e3945893198ad5
Skanda lin <anikiwayne@gmail.com>
2020-01-17 15:19:33 +0800

    mark and add note for api

--- src/api.js
+++ src/api.js
@@ -1,53 +1,78 @@
 export default {
+    /* 取得使用者資料API priority need ***** */
     api1: 'api/v1/auth/token',
     api2: 'api/v1/user/level/prop/history',
     api3: 'api/v1/user/me',
     api4: 'api/v1/user/buff',
     api5: 'api/v1/user/trade/history/',
+    /* 特約商店API priority need ** */
     api6: 'api/v1/contract_store',
     api7: 'api/v1/member/cash_trade/describe/',
     api8: 'api/v1/member/cash_trade',
     api9: 'api/v1/member/cash_trade/accept/',
     api10: 'api/v1/member/cash_trade/reject/',
     api11: 'api/v1/member/cash_trade/create',
+    /* 各大頁面BANNER API from Admin Dashboard priority need ***** */
     api12: 'api/v1/banner',
+    /* 使用者資料更改API priority need ***** */
     api13: 'api/v1/user/active/code/profile',
     api14: 'api/v1/user/profile',
     api15: 'api/v1/user/profile/password',
     api16: 'api/v1/member/',
     api17: 'api/v1/member/inviter/',
     api18: 'api/v1/member/active/code/register',
+    /* 遊戲API priority need ***** */
     api19: 'api/v1/game/record',
+    /* 組織圖API priority need ** */
     api20: 'api/v1/user/tree/',
     api21: 'api/v1/user/tree/fill/',
     api22: 'api/v1/user/tree/move/',
+    /* 忘記密碼API priority need ***** */
     api23: 'api/v1/member/forget/password',
     api24: 'api/v1/user/token',
+    /* 使用者好友API priority need ***** */
     api25: 'api/v1/user/friends',
     api26: 'api/v1/user/active/code/account',
+    /* 購買交易幣API priority need ***** */
     api27: 'api/v1/tradingPlatform/ucoinExclusive',
     api28: 'api/v1/tradingPlatform/history',
     api29: 'api/v1/tradingPlatform/ucoin/',
     api30: 'api/v1/tradingPlatform/ucoin/expenses',
     api31: 'api/v1/tradingPlatform/ucoin/show',
-    api32: 'api/v1/treasure/pool_total',
+    //depricated api32: 'api/v1/treasure/pool_total' depricated,
     api33: 'api/v1/tradingPlatform/ucoin/income',
+    /* 取得幣況API priority need **** */
     api34: 'api/v1/ucoin',
+    /* 取得使用者道具背包API priority need **** */
     api35: 'api/v1/user/backpack',
+    /* 取得商城API priority need **** */
     api36: 'api/v1/shop/store/show',
+    /* 使用道具卡片API priority need **** */
     api37: 'api/v1/user/backpack/item/',
+    /* 刪除訂單API priority need ***** */
     api38: 'api/v1/order/',
     api39: 'api/v1/order/retry/',
+    /* 最新消息API priority need ***** */
     api40: 'api/v1/news',
+    /* 使用無線發財卡API priority need ***** */
     api41: 'api/v1/member',
+    /* 信件夾API priority need ***** */
     api42: 'api/v1/user/inbox',
+    /* 建立訂單API priority need ***** */
     api43: 'api/v1/order',
+    /* 信件夾未讀API priority need ***** */
     api44: 'api/v1/user/inbox/pending',
+    /* 註冊簡訊API priority need ***** */
     api45: 'api/v1/user/active/code',
+    /* 註冊簡訊API priority need ***** */
     api46: 'api/v1/user/active',
+    /* 查詢使用道具API priority need **** */
     api47: 'api/v1/user/backpack/history/',
     api48: 'api/v1/shop/store/',
+    /* 簽到API priority need ***** */
     api49: 'api/v1/sign_in/check',
+    /* 遊戲API priority need ***** */
     api50: 'api/v1/game',
+    /* 遊戲API priority need ***** */
     api51: '/api/v1/treasure/token'
 }
